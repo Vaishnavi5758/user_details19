d663f8eb94c122cce344ad66b3138eb86dc4c75d
Vaishnavi5758 <vaishnavipatil5758@gmail.com>
2023-10-21 16:46:21 +0530

    delete item by id

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+
+
+
 const myForm = document.querySelector('#myForm');
 const amount = document.querySelector('#amount');
 const description = document.querySelector('#description');
@@ -25,7 +28,7 @@ function onsubmit(e){
       };
     
     
-      axios.post("https://crudcrud.com/api/27776cfaaf89452585b89847929eaf34/userData",user)
+      axios.post("https://crudcrud.com/api/f9b65d1342664beaa905477f70ec401d/userData",user)
       .then((response)=>{
           showNewUserOnScreen(response.data);
           console.log(response)
@@ -42,7 +45,7 @@ function onsubmit(e){
 
 window.addEventListener("DOMContentLoaded",()=>{
    
-    axios.get("https://crudcrud.com/api/27776cfaaf89452585b89847929eaf34/userData")
+    axios.get("https://crudcrud.com/api/f9b65d1342664beaa905477f70ec401d/userData")
     .then((response)=>{
 
         for(var i=0;i<response.data.length;i++){
@@ -100,12 +103,82 @@ function onDelete(e) {
         if(confirm('Are you sure?')){
             var li =e.target.parentElement.parentElement;
             console.log(li);
-          
-   // Remove the <li> element from the DOM
-   usersList.removeChild(li);
+
+            var textContent = '';
+
+            // Iterate through child nodes of the <li> element
+            for (var node of li.childNodes) {
+                if (node.nodeType === Node.TEXT_NODE) {
+                    textContent += node.textContent;
+                }
+            }
+    
+            // Trim the combined text content
+            textContent = textContent.trim();
+
+             // Split the itemText into parts to extract "amount" and "description"
+        const parts = textContent.split('-');
+
+        // Create an object with the data you want to send to the server
+        const finalText = {
+            amount: parts[0].trim(),
+            description: parts[1].trim(),
+            category:parts[2].trim(),
+        };
+            
+           // var itemText = li.textContent;
+            console.log(finalText);
+
+           // const searchText = 100100 - lucky- movie;
+   
+           // Fetch the data from the server and then delete the item
+      fetchItemsFromServer()
+      .then(data => {
+        const itemToDelete = findItemInData(data, finalText);
+        if (itemToDelete) {
+          deleteItemFromServer(itemToDelete._id)
+            .then(() => {
+              console.log('Item deleted successfully on the server.');
+              // Remove the <li> element from the DOM
+              li.remove();
+            })
+            .catch(error => {
+              console.log('Error deleting item on the server:', error);
+            });
+        } else {
+          console.log('Item not found in the server data. Unable to delete.');
         }
-    }
+      });
+  }
 }
+}
+
+// Function to fetch data from the server (replace with your server's logic)
+function fetchItemsFromServer() {
+return axios.get('https://crudcrud.com/api/f9b65d1342664beaa905477f70ec401d/userData')
+  .then(response => {
+    return response.data;
+  })
+  .catch(error => {
+    console.error('Error fetching data from the server:', error);
+    throw error;
+  });
+}
+
+// Function to find an item in the data based on finalText
+function findItemInData(data, finalText) {
+return data.find(item => item.amount === finalText.amount && item.description === finalText.description && item.category === finalText.category);
+}
+
+// Function to delete an item from the server
+function deleteItemFromServer(itemId) {
+return axios.delete(`https://crudcrud.com/api/f9b65d1342664beaa905477f70ec401d/userData/${itemId}`);
+}
+    
+    
+    
+    
+
 
 function onEdit(e) {
     // Get the parent <li> element when "Edit" is clicked
